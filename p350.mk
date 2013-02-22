e492e091127bc0871032b8fe0d5a1874661e0901
RonGokhale <cips173@gmail.com>
2013-02-22 18:50:13 +0530

    p350: we don't have full 3.0 msm_fb yet
    Change-Id: Ia7fa1048f481127391ce0566e8dcb7949f13c88a

--- p350.mk
+++ p350.mk
@@ -158,9 +158,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.enable-aac=true \
     media.stagefright.enable-qcp=true
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.pcm.version=2.0.1
-
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_DEVICE := p350
