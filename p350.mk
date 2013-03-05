dc3a4de0a2986191e036e652b24b28acb6606bd0
RonGokhale <cips173@gmail.com>
2013-03-05 20:11:34 +0530

    p350: Redone USB config for updated ICS android gadget
    Change-Id: I87e65a96bc4fa5fc1edc86fc63d5b9605cb1449f
    Signed-off-by: RonGokhale <cips173@gmail.com>

--- p350.mk
+++ p350.mk
@@ -56,7 +56,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab
 
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/AudioFilter.csv:system/etc/AudioFilter.csv \
@@ -64,6 +63,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
+
+# Misc 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh
 
 
 # Display 
@@ -131,8 +134,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Battery life hacks
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.ril.disable.power.collapse=1
-    pm.sleep_mode=1
+    ro.ril.disable.power.collapse=1 \
+    pm.sleep_mode=1 \
     wifi.supplicant_scan_interval=150
 
 PRODUCT_PROPERTY_OVERRIDES += \
